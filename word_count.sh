f5ab4701b617983303923c67a5a45a0fcf8b9fc7
mateus <mateus.aluufc@gmail.com>
2016-11-04 13:18:08 -0300

    Update word_count.sh

--- word_count.sh
+++ word_count.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+# Use this script on the server
+# 
+# Mateus-n00b, November 2016
+#
+# Version 1
+#
+# Licence GPL
+#--=============================================---
+ 
 sum=0
 num=0
 for x in novo* 
